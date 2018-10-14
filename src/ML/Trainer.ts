1e2d17bdded75ff88a27d260389087d9ee723723
Roberto Murguia <robertmalvarado@gmail.com>
2018-10-14 13:44:49 -0500

    Agregar evento cuando el trainer se connecta con mongodb

--- src/ML/Trainer.ts
+++ src/ML/Trainer.ts
@@ -1,18 +1,22 @@
 import Headset from '../Headset/Headset';
 import { MongoClient } from 'mongodb';
+import { EventEmitter } from 'events';
 
 class Trainer {
     
     protected headset: Headset;
     public collection: MongoClient.databse.collection;
+    protected emitter: EventEmitter;
 
     constructor(headset: Headset) {
         this.headset = headset;
         let client = new MongoClient('mongodb://127.0.0.1:27017');
+        this.emitter = new EventEmitter();
         client.connect(err => {
             if (err) return;
+            this.emitter.emit('connected');
             this.collection = client.db('vsense').collection('train');
-        })
+        });
     }
 
     public train(output: string, action: Promise<any>) {
@@ -26,6 +30,12 @@ class Trainer {
         }, 50);
 
         action.then(() => clearInterval(interval))
+    }
+
+    public on(event:string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.emitter.on(event, resolve);
+        })
     }
 
 }
